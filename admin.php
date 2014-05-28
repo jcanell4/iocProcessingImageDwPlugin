25f700974364f96b15f4f0055f353f7116d27730
Dani Criado <dani.criado.casas@gmail.com>
2014-05-28 07:56:30 +0200

    Error en el parser

--- admin.php
+++ admin.php
@@ -1,4 +1,4 @@
-    <?php
+        <?php
 /*
  *  User Manager
  *
@@ -13,8 +13,13 @@
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 
+if (!defined('DOKU_PLUGIN'))
+    define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
+if (!defined('DOKU_PROCESSING'))
+    define('DOKU_PROCESSING', DOKU_PLUGIN . "processingmanager/");
+
 require_once DOKU_INC.'inc/common.php';
-require_once DOKU_INC.'lib/plugins/processingmanager/generateImage.php';
+require_once DOKU_PROCESSING.'generators/generateImage.php';
 
 /**
  * All DokuWiki plugins to extend the admin function
@@ -70,9 +75,22 @@ class admin_plugin_processingmanager extends DokuWiki_Admin_Plugin {
         $generate = new generateImage();
         $generate->setChanges(array("@dani@"=>"dani"));
         $generate_html = $generate->getHtml();
-        $html = "<div style='width: 800; height: 600px'>"
-                . "<div id='generate'>".$generate_html."</div>"
-                . "</div>";
-        echo $html;
+        $dojo = 
+        "<script type='text/javascript'>"
+        ."require(["
+        . "'dojo/parser', "
+        . "'dijit/layout/TabContainer', "
+        . "'dijit/layout/ContentPane'"
+        . "]);</script>";
+        $html = ""
+        ."<div style='width: 800; height: 600px;'>"
+            . "<div data-dojo-type='dijit/layout/TabContainer' data-dojo-config=\"local='ca'\" style='width:100%; height: 100%;'>"
+                . "<div data-dojo-type='dijit/layout/ContentPane' data-dojo-config=\"local='ca'\" title='Generació de imatges' id='generateImage' >".$generate_html."</div>"
+                . "<div data-dojo-type='dijit/layout/ContentPane' title='Càrrega de algorismes' id='loadAlgorithm'>".$generate_html."</div>"
+                . "<div data-dojo-type='dijit/layout/ContentPane' title='Galeria de imatges' id='galleryImage'>".$generate_html."</div>"
+            . "</div>"
+        . "</div>"
+        . "";
+        echo $dojo . $html;
     }    
 }
---
+++ generators/generateImage.php
@@ -0,0 +1,21 @@
+<?php
+
+if(!defined('DOKU_INC')) die();
+
+if (!defined('DOKU_PLUGIN'))
+    define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
+if (!defined('DOKU_PROCESSING'))
+    define('DOKU_PROCESSING', DOKU_PLUGIN . "processingmanager/");
+
+
+require_once DOKU_PROCESSING.'abstractGenerateHtml.php';
+
+class generateImage extends abstractGenerateHtml {
+    
+    function generateImage() {
+        $this->setFile(DOKU_PROCESSING.'templates/generate.html');
+    }
+    
+}
+?>
+
