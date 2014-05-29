f5904d4a016b2550180e60c39895d26d6e44d69c
P2-JCanell4-82 <professor@P2-JCanell-84.(none)>
2014-05-29 12:05:11 +0200

    fix the relative path of jar file

--- conf/default.php
+++ conf/default.php
@@ -14,7 +14,7 @@ $conf['getPdeClassesURLParam']          = 'getPdeClassesURL';
 $conf['appletHeight']                   = '600';
 $conf['appletWidth']                    = '800';
 $conf['codeValue']                      = 'ioc.wiki.processingmanager.applet.ImageGeneratorApplet';
-$conf['appletJarName']                  = './lib/_java/processingManager.jar';//Perque amb ruta relativa?
+$conf['appletJarName']                  = 'lib/_java/processingManager.jar';//Perque amb ruta relativa?
 $conf['getPdeClassesInfoCommand']       = 'get_pde_classes_info';
 $conf['fileSenderCommand']              = 'save_unlinked_image&do=saveImage';
 $conf['nameSenderCommand']              = 'save_unlinked_image&do=existsImage';
